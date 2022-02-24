e1b3cce85c6c5240668dca7184178dac5911563e
Corine <cmsengineering@knights.ucf.edu>
2022-02-24 05:50:28 -0500

    Backup of Midterm Files

---
+++ Midterm_CSmith/planet_tour.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def planet_tour(start, destination):
+    """Function takes the travelers start location and destination location, and returns the travel information total distance and travel time.
+    Input: Start location(start) , destination location (destination)
+    Ouput: Travel time (year, day, hour, min) , travel distance( AU )"""
+    
+    Travel_time_offical = traveltime 
+    Travel_distance_offical = distance_for_traveller
+    
+    print("For your upcoming trip, the travel time to your destination will be: \n", Travel_time_offical, "\n", "Your distance travelled will be: \n", Travel_distance_offical)
+    
+    return Travel_time_offical , Travel_distance_offical
+
+
+# In[ ]:
+
+
+
+
