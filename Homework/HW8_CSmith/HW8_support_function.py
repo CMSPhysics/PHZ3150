5171d248af3580a59f262b5610af197fc9ac2adc
Corine <cmsengineering@knights.ucf.edu>
2022-03-17 05:54:54 -0400

    backup

--- Homework/HW8_CSmith/HW8_support_function.py
+++ Homework/HW8_CSmith/HW8_support_function.py
@@ -6,17 +6,23 @@ def kepler_3rd(planet_x, p1, p2, a1):
     Input: Planet of interest(planet_x), orbital period planet Earth(p1) days, orbital period planet x(p2) days, distane planet Earth(a1) AU
     Output: Orbital distance planet x(a2) AU"""
     
-    a2 = np.round(np.cbrt((np.power(a1, 3) * np.power(p2, 2))/np.power(p1, 2)), 2)
+    a2 = np.round(np.cbrt(((p2**2)*(a1**3))/(p1**2)), 2)
     
     print('The distance from the planet', planet_x, 'is', a2, 'AU.')
     
     return a2 
     
 def piston(V,P0,V0,T0,gamma):
-    """The function should then calculate the pressures for the piston volumes in V, using the adiabatic law. Then, the function should
-    calculate the temperatures using the ideal gas law."""
+    """Calculates the pressures for the piston volumes in V, using the adiabatic law, and calculates the temperatures using the ideal gas law.
+    Input: Volume(L), inital pressure (ATM), inital temperature (K), gamma 
+    Output: pressure(ATM), temperature(K)"""
+
+    const = (P0*V0)/T0
+    const1 = P0*(V0**gamma)
     
-    T = (P0 * V0) / C
-    P = np.power(C , 1.4) / V
+    P = const1/(V**gamma)
+    T = P*V/ const
+              
+    Piston = np.array([P, V, T])
     
-    return (np.array[P, V, T])
+    return Piston
